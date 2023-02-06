311d5d527992859b84f4a002ee9d8775dbd10335
mfaiz6 <Mohammadfaiz_2019bcse002@nitsri.net>
2023-02-06 15:14:01 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import Loading from './components/loading spinner/Loading'
 function App() {
+
+
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+
+  // useEffect(() => {
+  //   console.log(`Loading: ${loading}`);
+  //   console.log(`Data: ${JSON.stringify(data)}`);
+  // }, [loading, data]);
+
+
+  const handleClick = async () => {
+    setLoading(true)
+    try {
+      const response = await axios.get("https://reqres.in/api/users?page=1")
+      // console.log(response.data)
+      setData(response.data)
+
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false)
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <nav>
+        <div className="brand-name">MyUsers</div>
+        <button onClick={handleClick}>Get Users</button>
+      </nav>
+
+
+      {loading && <Loading />}
+
+
+      <div className="container">
+        {Array.isArray(data.data) && data.data.map((item) => (
+          <div className="card" key={item.id}>
+            <div className="email">{item.email}</div>
+            <div className="name">{item.first_name}</div>
+            <img src={item.avatar} alt="avatar" />
+          </div>
+        ))}
+      </div>
+
+
+    </>
   );
 }
 
